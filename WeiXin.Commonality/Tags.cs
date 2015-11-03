40d68677a8ea27f24c3017b8159d987e56b39c8e
yizhaoxian <nilaiyaowa@126.com>
2015-11-03 09:40:26 +0800

    提交项目框架

---
+++ WeiXin.Commonality/Tags.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeiXin.Commonality
+{
+    public class Tags
+    {
+        public const string AppId = "wx3bc542276f30a8b6";
+        public const string AppSecret = "b2036bf2db9121b0fddd5e69bbef2e25";
+        public const string Token = "liusl9633";
+
+    }
+}
---
+++ WeiXin.Web/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeiXin.Commonality;
+
+namespace WeiXin.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            ViewBag.appid = Tags.AppId;
+            ViewBag.appsecret = Tags.AppSecret;
+            ViewBag.token = Tags.Token;
+            return View();
+        }
+
+        public JsonResult UploadImage()
+        {
+            return Json(Tags.Token,JsonRequestBehavior.AllowGet);
+        }
+       
+    }
+}
